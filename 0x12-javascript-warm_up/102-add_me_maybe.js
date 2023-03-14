55ac8fab8785587602265904b10d7c9ddd079a26
Fashinray <111636176+Fashinray@users.noreply.github.com>
2023-03-14 15:55:34 +0100

    Update 102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
+
+function addMeMaybe (n, func) {
+  n++;
+  func(n);
+}
+module.exports = {
+  addMeMaybe: addMeMaybe
 };
-console.log(myObject);
-myObject.incr = function () {
-  this.value++;
-};
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
