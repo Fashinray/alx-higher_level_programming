1139e6b4b46fe9947329ac0f12b07c542e498400
Fashinray <111636176+Fashinray@users.noreply.github.com>
2023-03-15 01:26:56 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
+
 const dict = require('./101-data').dict;
 
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
+const entries = Object.entries(dict);
 const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
-    }
+for (const [key, value] of entries) {
+  if (value in newDict) {
+    newDict[value].push(key);
+  } else {
+    newDict[value] = [key];
   }
-  newDict[valsUniq[j]] = list;
 }
+
 console.log(newDict);
