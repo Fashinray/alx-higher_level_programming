5e3ad34b47cb7974329ae8aa1d5acf5103238851
Fashinray <111636176+Fashinray@users.noreply.github.com>
2023-03-16 01:21:05 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
-/**
- * a script that prints a message depending
- * on the number of arguments passed:
- * if no arguments are passed to the script, print “No argument”
- * if only one argument is passed to the script, print “Argument found”
- * Otherwise, print “Arguments found”
- */
-if (process.argv[2]) {
-  console.log(process.argv[2]);
-} else {
+if (!process.argv[2]) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
