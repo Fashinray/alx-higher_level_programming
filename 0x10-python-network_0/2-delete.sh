4e50d18bcca9a9a1def3e6d8f839c4f0a90287e8
Fashinray <Oluwatosin5383@gmail.com>
2023-04-02 04:43:56 +0100

    Networking

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,2 +1,3 @@
-end a DELETE request to a given URL and display the response body.
+#!/bin/bash
+# Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
